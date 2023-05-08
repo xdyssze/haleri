d319c1555cbc0cd61a7f54fb00a76fc5a78a1d64
xdyssze <53408336+xdyssze@users.noreply.github.com>
2023-05-08 08:57:38 +0200

    Funny

--- js/main.js
+++ js/main.js
@@ -1,42 +1,72 @@
+let items;
+
+let Page = [];
+window.addEventListener("load", (event) => {
+    (async () => {
+        items = await getItems();
+        Page.Items = items;
+        Page.generateContent();
+      })();   
+    console.log(items);
+});
 
 
+async function getItems() {
+    const response = await fetch("https://halerinodejs.roeappelqvist1.repl.co/get");
+    const jsonData = await response.json();
+    return jsonData;
+}
 
 
+Page.generateContent = function() {
+    console.log(Page.Items);
+  for(var i = 0; i < Page.Items.length; i++) {
+    var itembox = document.createElement("div");
+    var price = document.createElement("p");
+    var name = document.createElement("p");
+    var img = document.createElement("img");
+    //image
+    img.src = ("https://halerinodejs.roeappelqvist1.repl.co/img/" + Page.Items[i].imgid);
+    img.alt = ("Image of " + Page.Items[i].name);
+    img.className = "imgBox";
+    img.onclick = function() { Page.createItemWindow(this.id); };
+    //price
+    price.innerHTML = (Page.Items[i].price + "kr");
+    price.className = "price";
+    //name
+    name.innerHTML = (Page.Items[i].name);
+    name.className = "name";
+    //itembox
+    itembox.className = "item";
+    itembox.id = (i);
+    //lägger inn allt i itemboxen innan man slutligen lägger in den i containern
+    itembox.append(img);
+    itembox.append(price);
+    itembox.append(name);
+    document.querySelector("#main").append(itembox);
+  }
+}
+Page.clearItemFrame = function() { 
+    var main = document.querySelector("#main");
+    while(main.childNodes.length > 0) {
+        for(let i = 0; i < (main.childNodes.length); i++) { 
+            main.childNodes[i].remove();
+        }
+    }
+}
+Page.createItemWindow = function(id) {
+    Page.clearItemFrame();
+    var mainf = document.createElement("div");
+    var img = document.createElement("img");
+    var purchaseButton = document.createElement("img");
 
 
-
-
+}
+function debug() {
+    sex();
+}
 function expandCart() {
-    fetch("https://halerinodejs.roeappelqvist1.repl.co/get", {
-        Method: 'GET',
-        Headers: {
-          Accept: 'application.json',
-           'Content-Type': 'application/json'
-        },
-        Body: body,
-        Cache: 'default'
-    })
-    .then((response) => {
-        console.log(response);
-    })
-    .then((data) => {
-
-        console.log(data);
-    })
-    .catch(error => {
-        console.log(error);
-    });
+    cons
 }
 // jag vet johan, jag kan säkert bara använda arrays / jsonObjekt för detta men jag gillar att komplicera saker, om du inte redan har märkt det :)
 
-
-class Item {
-    name;
-    id;
-    img;
-    pris;
-    kategori;
-    desc;
-    constructor() {
-    }
-}
